42118cf57519539ff78828ebaa2a50226497277c
Castiel <castiel.andrew@gmail.com>
2018-03-27 14:54:36 -0700

    Basic set up with all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { InfoComponent } from './info/info.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    InfoComponent,
+    ProjectsComponent,
+    ProjectDetailComponent,
+    ProjectListComponent,
+    GalleryComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
